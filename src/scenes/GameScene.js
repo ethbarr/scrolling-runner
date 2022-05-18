52f73d1164fe8c5c718fea5e6150641905e07e3e
Ethan Barrett <45881739+ethbarr@users.noreply.github.com>
2022-05-18 16:21:19 -0400

    Initial commit

---
+++ src/scenes/GameScene.js
@@ -0,0 +1,29 @@
+import Phaser from 'phaser'
+import ScoreLabel from '../ui/ScoreLabel.js'
+
+
+export default class GameScene extends Phaser.Scene
+{
+	constructor()
+	{
+		super('game-scene')
+	}
+
+	preload()
+    {
+
+    }
+
+    create()
+    {
+
+    }
+
+
+	update()
+	{
+
+	}
+
+
+}
